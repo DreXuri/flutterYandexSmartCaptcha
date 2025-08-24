535339510c666436a92b4bba4416e1577c78d17f
DreXuri <balogundamilaree@gmail.com>
2025-08-24 23:17:34 +0100

    init

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sergejafanasov/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sergejafanasov/flutter_projects/flutter_yandex_smartcaptcha"
+export "FLUTTER_ROOT=/Users/drexuri/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/drexuri/Desktop/my-app/captch/flutterYandexSmartCaptcha"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.7"
 export "FLUTTER_BUILD_NUMBER=0.0.7"
+export "FLUTTER_CLI_BUILD_MODE=debug"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
